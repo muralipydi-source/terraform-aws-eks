2680a386267e7ef03860c430ee6401ac819b2e1a
murali <murali.pydi@gmail.com>
2025-12-02 13:11:59 +0530

    code change in files

---
+++ 80-eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01602722QJVZMCD67LWU"
+}
+
+variable "zone_name" {
+    default = "muraliitemp.online"
+}
