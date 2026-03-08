b6badcc25f291503a4250bdd373bcef71063e759
murali <murali.pydi@gmail.com>
2026-03-08 09:46:17 +0530

    code change in files

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -5,12 +5,9 @@ resource "aws_iam_role" "ec2_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid    = ""
+        "Effect": "Allow",
+         "Action": "*",
+         "Resource": "*"
       }
     ]
   })
